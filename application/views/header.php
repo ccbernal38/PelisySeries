19f18f794afa6dd11fdde97b438e8594c37a74cb
DanielLoaiza <danielfelipeloaiza@hotmail.com>
2015-04-29 21:32:06 -0500

    Implementación de codeigniter
    Todos los archivos fueron reubicados en las respectivas carpetas para el
    framework, para acceder a una vista no se puede directamente por la url,
    la vista debe ser llamada desde un controlador ( ver ejemplo )
    http://localhost/codeigniter/index.php/welcome/prueba

---
+++ application/views/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<script src="<?php echo base_url(); ?>js/jquery-2.1.3.js"></script>
+	<script src="<?php echo base_url(); ?>js/bootstrap.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/nuevo.css">
+	<script src="<?php echo base_url(); ?>js/tmdb.js"></script>
+	<script src="<?php echo base_url(); ?>js/basic.js"></script>
+	<?php
+	//include '../libs/facebook/app/start.php';
+	?>
+	<title>Informacion de peliculas y series</title>
+</head>
+<body>	
+	<header id="main-header">
+		<div class="container ">
+			<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+				<div class="row ">
+					<div class="col-xs-3 col-xs-offset-1">
+						<a id="logo" class="navbar-brand" >
+							<img  src="<?php echo base_url(); ?>img/logo.png" class="media-heading img-responsive" alt="PelisySeries">
+						</a>
+					</div>			
+					<div class="col-xs-3 col-xs-offset-1">
+						<ul class="nav nav-pills">
+							<li class="active"><a href="">Principal</a></li>
+							<li><a href="">Peliculas</a></li>
+							<li><a href="">Series</a></li>
+						</ul>
+					</div>	
+					<div class="col-xs-3">
+						<?php if (!isset($_SESSION['facebook'])): ?>
+						<ul class="nav nav-pills">
+							<li>
+								<a href="login.php">Login</a>
+							</li>
+							<li>
+								<a href="registro.php">Registrarse</a>
+							</li>
+						</ul>
+					<?php else: ?>
+						<ul class="nav nav-pills">
+							<li>
+								<a href="#" class="btn-danger" title=""><?php echo $facebook_user->getName();?></a>								
+							</li>
+							<li>
+								<a href="../libs/facebook/app/logout.php">Cerrar sesión</a>
+							</li>
+						</ul>
+					<?php endif; ?>
+					</div>
+				</div>	
+			</nav>	
+		</div>		
+	</header><!-- /header -->
