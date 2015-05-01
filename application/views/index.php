3f5323c7d822cdb49c395d2d9dde4a41887bc941
ccbernal38 <bernal_9219@hotmail.com>
2015-05-01 14:59:07 -0500

    index

--- application/views/index.php
+++ application/views/index.php
@@ -1,3 +1,4 @@
+	<?php include 'header.php';	?>
 	<script src="<?php echo base_url(); ?>js/principal.js" type="text/javascript"></script>
 	<section class="fondo col-xs-12">
 		<div class="container">
